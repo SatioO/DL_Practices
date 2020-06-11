7d9e6f1c8aeb54b28af1b8c3a346b47338008e57
SatioO <vaibhav.satam29991@gmail.com>
2020-06-11 19:10:55 +0530

    added data pipeline

---
+++ cityscapes/dataset.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+def data_generator(images, masks, is_training, img_height=224, img_width=224, batch_size=16, buffer_size=5000):
+    def parse_fn(img_path, mask_path):
+        img = tf.io.read_file(img_path)
+        img = tf.cast(tf.image.decode_png(img, channels=3), tf.float32)
+        img = tf.image.resize(img, [img_height, img_width])
+        img = tf.clip_by_value(img, 0, 255)
+        img = tf.image.per_image_standardization(img)
+
+        mask = tf.io.read_file(mask_path)
+        mask = tf.image.decode_png(mask, channels=1)
+        mask = tf.cast(tf.image.resize(
+            mask, [img_height, img_width]), tf.uint8)
+
+        return img, mask
+
+    def flip(img, mask):
+        img = tf.image.random_flip_left_right(img, seed=1)
+        mask = tf.image.random_flip_left_right(mask, seed=1)
+
+        return img, mask
+
+    def color(img, mask):
+        img = tf.image.random_hue(img, 0.05)
+        img = tf.image.random_saturation(img, 0.4, 1.2)
+        img = tf.image.random_brightness(img, 0.05)
+        img = tf.image.random_contrast(img, 0.4, 1.2)
+
+        return img, mask
+
+    dataset = tf.data.Dataset.from_tensor_slices((images, masks))
+
+    if is_training:
+        dataset = dataset.shuffle(buffer_size)
+    dataset = dataset.map(
+        parse_fn, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    augmentations = [flip, color]
+
+    if is_training:
+        for f in augmentations:
+            if tf.random.uniform([1], 0, 1) > 0.6:
+                dataset = dataset.map(
+                    f, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    dataset = dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
+
+    return dataset
--- cityscapes/main.py
+++ cityscapes/main.py
@@ -1,19 +1,21 @@
 import os
-import glob
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from dataset import data_generator
+
+tf.random.set_seed(42)
 
 data_dir = "../data/cityscapes/"
 
-train_imgs = sorted(glob.glob(os.path.join(
+train_imgs = sorted(tf.io.gfile.glob(os.path.join(
     data_dir, "leftImg8bit/train", "*/*.png")))
-train_masks = sorted(glob.glob(os.path.join(
+train_masks = sorted(tf.io.gfile.glob(os.path.join(
     data_dir, "gtFine/train", "*/*_gtFine_labelIds.png")))
 
-val_imgs = sorted(glob.glob(os.path.join(
+val_imgs = sorted(tf.io.gfile.glob(os.path.join(
     data_dir, "leftImg8bit/val", "*/*.png")))
-val_masks = sorted(glob.glob(os.path.join(
+val_masks = sorted(tf.io.gfile.glob(os.path.join(
     data_dir, "gtFine/val", "*/*_gtFine_labelIds.png")))
 
 print('Found', len(train_imgs), 'training images')
@@ -22,25 +24,14 @@ print('Found', len(val_imgs), 'validation images\n')
 img_width = 224
 img_height = 224
 
-
-def read_img(img_path, mask_path):
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_png(img, channels=3)
-    img = tf.image.resize(img, [img_height, img_width])
-    img = tf.image.per_image_standardization(img)
-
-    mask = tf.io.read_file(mask_path)
-    mask = tf.image.decode_png(mask, channels=1)
-    mask = tf.cast(tf.image.resize(mask, [img_height, img_width]), tf.uint8)
-    return img, mask
-
-
-train_dataset = tf.data.Dataset.from_tensor_slices((train_imgs, train_masks))
-train_dataset = train_dataset.map(
-    read_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
 if __name__ == "__main__":
-    for i in train_dataset.take(4):
+    # Prepare data for training
+    train_batches = data_generator(
+        train_imgs, train_masks, is_training=True, img_height=img_height, img_width=img_width)
+    val_batches = data_generator(
+        val_imgs, val_masks, is_training=False, img_height=img_height, img_width=img_width)
+
+    for img in train_batches.take(4):
         fig, ax = plt.subplots(1, 2, figsize=(12, 12))
-        ax[0].imshow(i[0])
-        ax[1].imshow(np.reshape(i[1], (img_height, img_width)))
+        ax[0].imshow(img[0][1])
+        ax[1].imshow(np.reshape(img[1][1], (img_height, img_width)))
